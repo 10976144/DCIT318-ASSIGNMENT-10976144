567d90aa3f385d15872fa7f8e410980d3f4dcb90
10976144 <gkkorley@st.ug.edu.gh>
2024-06-17 13:51:06 +0000

    edited on commits

--- obj/Debug/net8.0/DCIT318-ASSIGNMENT.AssemblyInfo.cs
+++ obj/Debug/net8.0/DCIT318-ASSIGNMENT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DCIT318-ASSIGNMENT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e1cc24603292d1f82214ceb1ee3a3f794de03b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35fadcc52f122f03d73cb562da4e8ada66cf5297")]
 [assembly: System.Reflection.AssemblyProductAttribute("DCIT318-ASSIGNMENT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DCIT318-ASSIGNMENT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
